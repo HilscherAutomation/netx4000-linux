895b5de3a34e2d0cec22b6186e8bb0c3545baaa4
FME <github@hilscher.com>
2017-08-21 07:40:37 +0200

    pinctrl-single: Prevent simultaneous use of the same pin for GPIO and another function.

--- drivers/pinctrl/pinctrl-single.c
+++ drivers/pinctrl/pinctrl-single.c
@@ -402,6 +402,7 @@ static const struct pinmux_ops pcs_pinmux_ops = {
 	.get_function_groups = pinmux_generic_get_function_groups,
 	.set_mux = pcs_set_mux,
 	.gpio_request_enable = pcs_request_gpio,
+	.strict = true,
 };
 
 /* Clear BIAS value */
